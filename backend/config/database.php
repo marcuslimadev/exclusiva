e7af6152e0b78e77f21347422620348da496c6c2
Marcus André Lima de Lima <marcus.lima@hotmail.com.br>
2025-11-13 12:48:59 -0300

    fix: Adiciona configuração database e app para Render

---
+++ backend/config/database.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'default' => env('DB_CONNECTION', 'pgsql'),
+    
+    'connections' => [
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', 5432),
+            'database' => env('DB_DATABASE', 'crm_exclusiva'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+    ],
+];
